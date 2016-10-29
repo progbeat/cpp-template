510b04dddbbeb79b5aa01b1f0f65795e62f53f86
Roman Rizvanov <progbeat@gmail.com>
2016-10-29 13:49:35 +0300

    Added test2.

--- recurrence.cpp
+++ recurrence.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define UN(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
-#define FOR(i, a, b) for (int i(a), _B_##i(b); i < _B_##i; ++i)
+#define FOR(i, a, b) for (decltype(a) i(a), _B_##i(b); i < _B_##i; ++i)
 #define CL(a, b) memset(a, b, sizeof a)
 #define all(a) (a).begin(), (a).end()
 #define REP(i, n) FOR(i, 0, n)
@@ -9,15 +9,12 @@
 #define pb push_back
 #define Y second
 #define X first
-#ifndef LOCAL
-#define NDEBUG
-#endif
 
 using namespace std;
 
 typedef pair<int, int> pii;
 
-const int mod = 1e9 + 7;
+int mod;
 
 int add(int x, int y) { return (x += y) < mod ? x : x - mod; }
 int sub(int x, int y) { return (x -= y) < 0 ? x + mod : x; }
@@ -40,27 +37,38 @@ struct LinearRecurrence {
     vector<X> s, f = {1}, g = {1};
     int m = 1;
     X q = 1;
-
-    vector<X> pmul(vector<X> &a, vector<X> &b) {
+    
+    void preduce(vector<X> &a) const {
+        for (int n = sz(a); n -->= sz(f); a.pop_back()) {
+            FOR (i, 1, sz(f))
+                a[n - i] = sub(a[n - i], mul(a.back(), f[i]));
+        }
+    }
+    
+    vector<X> pmul(vector<X> &a, vector<X> &b) const {
         vector<X> r(sz(a) + sz(b));
         REP (i, sz(a)) REP (j, sz(b)) {
             r[i + j] = add(r[i + j], mul(a[i], b[j]));
         }
-        for (int n = sz(r); n -->= sz(f); r.pop_back()) {
-            FOR (i, 1, sz(f))
-                r[n - i] = sub(r[n - i], mul(r.back(), f[i]));
-        }
+        preduce(r);
         return r;
     }
 
-    X solve(ll n) {
+    X solve(ll n) const { return solve(n, n + 1)[0]; }
+    
+    vector<X> solve(ll from, ll to) const {
         vector<X> x = {0, 1}, r = {1};
-        for (; n; n /= 2) {
+        for (ll n = from; n; n /= 2) {
             if (n & 1) r = pmul(r, x);
             x = pmul(x, x);
         }
-        X res = 0;
-        REP (i, sz(r)) res = add(res, mul(r[i], s[i]));
+        vector<X> res;
+        FOR (n, from, to) {
+            res.emplace_back();
+            REP (i, sz(r)) res.back() = add(res.back(), mul(r[i], s[i]));
+            r.emplace(r.begin());
+            preduce(r);
+        }
         return res;
     }
     
@@ -78,29 +86,61 @@ struct LinearRecurrence {
             q = p;
             m = 1;
         } else if (++m > 128) {
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 };
 
-int main() {
+void test1() {
+    mod = 1e9 + 7;
     LinearRecurrence<int> a;
-    auto f = [](int x) {
-        return qpow(x, x % 10);
+    auto f = [](ll x) {
+        return qpow(x % mod, x % 10);
     };
-    for (int x = 0; ; ++x) {
-        if (a.next(f(x))) {
-            cout << x << endl;
-            break;
-        }
+    for (int x = 0; a.next(f(x)); ++x);
+    assert(sz(a.f) == 101);
+    REP (i, 100) {
+        ll x = 1000000ll + i * 65537;
+        assert(f(x) == a.solve(x));
     }
-    for (int x : a.f)
-        cout << x << ' ';
-    cout << endl;
-    REP (i, 20) {
-        int x = 1000000 + i * 123;
-        cout << i << ' ' << f(x) << ' ' << a.solve(x) << endl;
+}
+
+uint32_t xorshift128() {
+    static uint32_t x = 123456, y = 234567, z = 345678, w = 456789;
+    uint32_t t = x ^ (x << 11);
+    t ^= t >> 8;
+    x = y; y = z; z = w;
+    return w ^= (w >> 19) ^ t;
+}
+
+void test2() {
+    mod = 2;
+    LinearRecurrence<int> r;
+    ll n = 0;
+    for (; ; ) {
+        uint32_t x = xorshift128();
+        bool ok = true;
+        REP (i, 32) ok &= r.next((x >> i) & 1);
+        n += 32;
+        if (!ok) break;
     }
+    const int SKIP = 10000000;
+    const int N = 100;
+    REP (k, SKIP) {
+        xorshift128();
+        n += 32;
+    }
+    vector<int> c = r.solve(n, n + 32 * N);
+    REP (k, N) {
+        uint32_t x = 0;
+        REP (i, 32) x |= c[k * 32 + i] << i;
+        assert(xorshift128() == x);
+    }
+}
+
+int main() {
+    test1();
+    test2();
     return 0;
 }
