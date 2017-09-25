d8e5bce2d418299f1970ebb5454ed3211e64c592
Roman Rizvanov <progbeat@gmail.com>
2017-09-25 11:46:36 +0300

    Minor fixes.

--- .test/ring.cpp
+++ .test/ring.cpp
@@ -2,7 +2,7 @@
 #include <math.hpp>
 
 using gf2 = quotient_ring_element<uint32_t, 2>;
-using gf1000000007 = quotient_ring_element<uint32_t, 256>;
+using gf1000000007 = quotient_ring_element<uint32_t, 1000000007>;
 
 int main() {
     gf1000000007 x, y;
